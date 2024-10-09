7efe2037d856a73b93ae68a4987181b4220dd348
tskeens71 <tskeens5@ivytech.edu>
2024-10-08 21:28:14 -0400

    lesson-2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://thomas6:ghoStly71@@cluster0.oe6ik.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://tskeens71:dante71@cluster0.oe6ik.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
